7e46b54e3ee150a6deb86f166c7f15ef50829cc4
MR7-GH <m.gh138080@gmail.com>
2024-12-19 19:19:36 +0330

    some responsivity bbugs fixed, and pages are connected now

--- panel2/Full-Scan/script.js
+++ panel2/Full-Scan/script.js
@@ -1,1 +1,15 @@
-// Nope. This is just the UI
+const searchButton = document.getElementById("submit-button");
+const searchLoader = document.getElementById("search-loader");
+const searchHandler = document.getElementById("search-object");
+
+
+    // Add the click event listener
+searchButton.onclick = () => {
+      // Toggle the "none-show" class on the loader
+      setTimeout(() => {
+        searchLoader.classList.remove("none-show");
+      }, 500);
+};
+searchHandler.onclick = () => {
+    searchLoader.classList.add("none-show");
+};
